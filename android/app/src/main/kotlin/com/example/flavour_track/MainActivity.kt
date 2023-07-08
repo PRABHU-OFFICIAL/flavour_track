bc38fadac29a71576f3ade95d811c5ae45df9dcf
Prabhu Prasad Penthoi <lituplayer@gmail.com>
2023-07-08 20:03:24 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flavour_track/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flavour_track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flavour_track/pages/homePage.dart';
+import 'package:flavour_track/signIn.dart';
+import 'package:flavour_track/signUp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
